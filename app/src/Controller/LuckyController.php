b6630eb2562be7e7ef6e4e9657ad779a0334b022
Tiago Montes <tiagosantos6303@gmail.com>
2022-11-08 14:41:04 -0500

    error GET

--- app/src/Controller/LuckyController.php
+++ app/src/Controller/LuckyController.php
@@ -2,18 +2,29 @@
 // src/Controller/LuckyController.php
 namespace App\Controller; // namespace é o caminho do arquivo
 
+use App\Entity\Filme;
+use App\Repository\FilmeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController extends AbstractController 
+class LuckyController extends AbstractController
 {
-    public function number(): Response // number é uma função que retorna um objeto response 
+    public function __construct( private FilmeRepository $filmeRepository, private EntityManagerInterface $em ) { // construtor é o trecho de código que é executado junto com a instanciação da classe
+    }
+    
+    #[Route('/filme', name: 'filme_list', methods: ['GET'])] // rota é o caminho que o usuário acessa
+    public function filmes(): Response 
     {
-        $number = random_int(0, 100); // $number é uma variável que recebe um número aleatório entre 0 e 100
 
-        return $this->render('lucky/number.html.twig', [ // retorna um objeto response com o template number.html.twig
-            'number' => $number, // passa a variável number para o template 
-        ]);
+        $filme = new Filme();
+        $filme->setName("Onde os fracos tem vez");
+        $this->em->persist($filme); // this faz ele procurar em toda a classe // persist faz ele salvar no banco de dados
+        $this->em->flush(); // flush atualiza o banco de dados
+        $filmes = $this -> filmeRepository -> findAll();
+        dd($filmes);
+
     }
 }
 
---
+++ app/src/Repository/FilmeRepository.php
@@ -0,0 +1,67 @@
+<?php
+// Resumindo, esse repositorio é responsavel pela tabela de Filmes
+
+namespace App\Repository;
+
+use App\Entity\Filme;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Filme>
+ *
+ * @method Filme|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Filme|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Filme[]    findAll()
+ * @method Filme[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class FilmeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Filme::class);
+    }
+
+    public function save(Filme $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Filme $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Filme[] Returns an array of Filme objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('f')
+//            ->andWhere('f.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('f.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Filme
+//    {
+//        return $this->createQueryBuilder('f')
+//            ->andWhere('f.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
