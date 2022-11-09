2e999af342d1f1eb002b857852a65f73a7457c41
Tiago Montes <tiagosantos6303@gmail.com>
2022-11-09 09:37:35 -0500

    adicionando estilizacoes e comentarios

--- app/src/Controller/FilmeController.php
+++ app/src/Controller/FilmeController.php
@@ -14,18 +14,18 @@ class FilmeController extends AbstractController
     public function __construct( private FilmeRepository $filmeRepository ) { // construtor é o trecho de código que é executado junto com a instanciação da classe
     }
     
-    #[Route('/filme', name: 'filme_list', methods: ['GET','POST'])] // rota é o caminho que o usuário acessa
-    public function filmes(Request $request): Response
+    #[Route('/filme', name: 'filme_list', methods: ['GET','POST'])] // rota é o caminho que o usuário acessa. O metodo GET é padrão, e post é quando o usuário envia dados
+    public function filmes(Request $request): Response // request é o objeto que contém os dados da requisição 
     {
 
-        if($request->request->all()){
+        if($request->request->all()){ // all() significa que ele vai pegar todos os dados da requisição e jogar no array
             $filme = new Filme();
-            $filme->setName($request->request->get('filme')); // seta o nome do filme
+            $filme->setName($request->request->get('filme')); // seta o nome do filme com o valor do input filme
             $this->filmeRepository->save($filme, true);// salva o filme
         }
 
-        return $this->render('lucky/number.html.twig', [ // retorna um objeto response com o template number.html.twig
-            'filmes' => $this->filmeRepository->findAll()
+        return $this->render('lucky/number.html.twig', [ // renderiza a view number.html.twig.
+            'filmes' => $this->filmeRepository->findAll() // 'filmes' é o nome da variável que vai ser usada no template, ele irá entrar no banco de dados e pegar todos os filmes 
         ]);
     }
 }
